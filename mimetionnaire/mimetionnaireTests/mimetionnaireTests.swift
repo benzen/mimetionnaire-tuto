29394a4fcfcf6cfd0ddee3d40af31ce9954ded1b
Benjamin Dreux <benjamin.dreux@code3.ca>
2014-10-24 13:34:04 -0400

    premiere vue avec NavigationController

---
+++ mimetionnaire/mimetionnaireTests/mimetionnaireTests.swift
@@ -0,0 +1,36 @@
+//
+//  mimetionnaireTests.swift
+//  mimetionnaireTests
+//
+//  Created by Benjamin Dreux on 2014-10-24.
+//  Copyright (c) 2014 Benjamin Dreux. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class mimetionnaireTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
